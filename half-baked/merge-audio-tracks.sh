b4ff78defb4d4062f6d33af99f4fb9f53aa0f874
Srikant Patnaik <srikant@fossee.in>
2013-09-10 10:59:12 +0530

    added multitrack audio support

---
+++ half-baked/merge-audio-tracks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# sudo apt-get install libavcodec-extra-53 ffmpeg ffmpeg2theora
+
+# first convert ogv to avi to extract audio
+#ffmpeg -i Ubuntu-Desktop-Bengali.ogv -vcodec mpeg4 -sameq -acodec libmp3lame B.avi
+
+# extract audio as mp3 from avi file
+#mplayer -dumpaudio -dumpfile B.mp3 B.avi
+
+#ffmpeg -y -i Ubuntu-Desktop-Bengali.ogv -vcodec copy B.mkv
+
+#reduce the size of original audio file by restricting it to mono and bitrate
+ffmpeg -i Ubuntu-Desktop-Telugu.ogv -y -b 14k -ac 1 -ar 11500 T.mp3
+ffmpeg -i Ubuntu-Desktop-Bengali.ogv -y -b 14k -ac 1 -ar 11500 B.mp3  #or use below one to convert to ogg
+ffmpeg -i Ubuntu-Desktop-Telugu.ogv -y -b 12k -ac 1 -ar 11500 -vn -acodec libvorbis T.ogg
+
+#ffmpeg -y -i B.mkv -i B.mp3 -i T.mp3 -map 0.0 -map 1.0 -map 2.0 -b 200k -vcodec copy -acodec copy out.mkv -newaudio
+
+ mkvmerge -o "linux-all-lang.ogv" -A Ubuntu-Desktop-English.ogv E.mp3 B.mp3 T.mp3 G.mp3 K.mp3 MM.mp3 M.mp3 U.mp3
